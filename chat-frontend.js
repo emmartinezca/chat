a3bd068d56e702570bfbd1fd84c2d7e33a6c5cdc
emmartinezca <emmartinezca@gmail.com>
2017-11-17 15:30:50 -0600

    started implementation of bootstrap 4

--- chat-frontend.js
+++ chat-frontend.js
@@ -29,7 +29,7 @@ $(function () {
     connection.onopen = function () {
         // first we want users to enter their names
         input.removeAttr('disabled');
-        status.text('Choose name:');
+        status.text('Elige un nombre:');
     };
 
     connection.onerror = function (error) {
